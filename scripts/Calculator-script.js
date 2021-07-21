c05d636cbd19fe9d0124ca81a27b9d97533f08d5
solenemhep <solene.pettier@gmail.com>
2021-07-21 20:21:42 +0200

    deployed

--- scripts/Calculator-script.js
+++ scripts/Calculator-script.js
@@ -2,20 +2,18 @@ const hre = require('hardhat');
 const { deployed } = require('./deployed');
 const { getContract } = require('./getContract');
 
-const TOKEN_PRICE = 100;
-
 async function main() {
   const [deployer, reserve, owner] = await hre.ethers.getSigners();
   console.log('Deploying contracts with the account:', deployer.address);
 
   const tokenAddress = await getContract('Token', 'kovan');
   // We get the contract to deploy
-  const ICO = await hre.ethers.getContractFactory('ICO');
-  const ico = await ICO.deploy(tokenAddress, owner.address, TOKEN_PRICE);
+  const Calculator = await hre.ethers.getContractFactory('Calculator');
+  const calculator = await Calculator.deploy(tokenAddress, owner.address);
 
-  await ico.deployed();
+  await calculator.deployed();
 
-  await deployed('ICO', hre.network.name, ico.address);
+  await deployed('Calculator', hre.network.name, calculator.address);
 }
 
 main()
